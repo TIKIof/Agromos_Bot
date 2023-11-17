64bb46b5710b72851e0a6e049d4ad93438d440d3
SNT Oficial <124302047+TIKIof@users.noreply.github.com>
2023-11-16 23:04:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['5214531287294', 'CuriosityBot-MD 🍧', true], ['5492266466080', 'Colaborado', true], ['5217294888993'], ['5492266466080'], ['593968585383'], ['51992004117'], ['5492266613038'], ['573106040746'], ['50558124470'], ['5214437863111'], ['51929972576']]
+global.owner = [['595994110242', 'Agromos SP Bot', true], ['5492266466080', 'Colaborado', true], ['5217294888993'], ['5492266466080'], ['593968585383'], ['51992004117'], ['5492266613038'], ['573106040746'], ['50558124470'], ['5214437863111'], ['51929972576']]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
@@ -14,12 +14,12 @@ global.suittag = ['994400359205']
 global.mods = []
 global.prems = []
 
-global.packname = 'CuriosityBot-MD'
+global.packname = 'AgromosSPBotMD'
 global.author = ''
-global.wm = '© CuriosityBot-MD'
-global.wm2 = '𝗖𝗎𝗋𝗂𝗈𝗌𝗂𝗍𝗒 : 𝗕𝗈𝗍'
-global.azami = 'Azami'
-global.cb = 'CuriosityBot-MD'
+global.wm = '© Agromos Bot'
+global.wm2 = 'Agromos SP : 𝗕𝗈𝗍'
+global.azami = 'SNT'
+global.cb = 'AgromosBotMD'
 
 global.vs = '1.0.2'
 global.library = 'Baileys'
